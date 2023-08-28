5e86fbf66345fad5141bb3a1067817049ed516e8
Michel Fedde <michel.fedde@gmail.com>
2023-08-28 19:28:46 +0200

    Tryied again the auto install of winget

--- run.ps1
+++ run.ps1
@@ -30,21 +30,8 @@ function installPrograms() {
         $wingetExists = Get-Command 'winget' -ErrorAction SilentlyContinue;
         if (!$wingetExists) {
             Write-Progress -Activity "Installing WinGet programs..." -CurrentOperation "Installing: WinGet"
-            
-            $URL = "https://api.github.com/repos/microsoft/winget-cli/releases/latest"
-            $URL = (Invoke-WebRequest -Uri $URL).Content | ConvertFrom-Json |
-                    Select-Object -ExpandProperty "assets" |
-                    Where-Object "browser_download_url" -Match '.msixbundle' |
-                    Select-Object -ExpandProperty "browser_download_url"
 
-            # download
-            Invoke-WebRequest -Uri $URL -OutFile "Setup.msix" -UseBasicParsing
-
-            # install
-            Add-AppxPackage -Path "Setup.msix"
-
-            # delete file
-            Remove-Item "Setup.msix"
+            Add-AppxPackage -RegisterByFamilyName -MainPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe
         }
 
         foreach ($program in $programs.winget) {
